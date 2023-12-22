f74e7366ba60c6d6cb303f6df8ad63b77cab0dc1
Paulina Kattilavaara <paulinakatt@hotmail.com>
2023-12-22 12:47:32 +0100

    candle event and position

--- js/candle.js
+++ js/candle.js
@@ -3,5 +3,6 @@ const candle = document.getElementById("candle-image");
 console.log(candle);
 // add event listener
 candle.addEventListener("click", () => {
-    
+    // document.body.classList.add("fire");
+    alert("NOOOOOOOO! Det brinner!!!!!!!! 🔥🔥🔥🔥🔥🔥🔥 HÄMTA BRANDSLÄCKAREN!!!!!!!!!!! 🧯🧯🧯🧯🧯🧯🧯");
 })
