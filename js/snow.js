bd3363c96c67be20ce383b775294b53ad98b5a6f
Alicia <aliciaxd@live.se>
2023-12-22 10:57:29 +0100

    fixed snow

--- js/snow.js
+++ js/snow.js
@@ -1,23 +1,28 @@
-function createSnowflakes() {
-  const numFlakes = 100; //antalet snöflingor
-  const cardBottom = document.querySelector(".card-bottom");
+document.addEventListener("DOMContentLoaded", function () {
+  function createSnowflakes() {
+    const numFlakes = 100; // Antalet snöflingor
+    const snow = document.getElementById("snow");
 
-  for (let i = 0; i < numFlakes; i++) {
-    const snowflake = document.createElement("div");
-    snowflake.className = "snowflake";
-    snowflake.innerHTML = "❄";
+    if (snow) {
+      // Kontrollera om #snow-elementet finns
+      for (let i = 0; i < numFlakes; i++) {
+        const snowflake = document.createElement("div");
+        snowflake.className = "snowflake";
+        snowflake.innerHTML = "❄";
 
-    const size = Math.random() * 20 + 10; // Storlek på snöflingor
-    snowflake.style.fontSize = `${size}px`;
+        const size = Math.random() * 20 + 10; // Storlek på snöflingor
+        snowflake.style.fontSize = `${size}px`;
 
-    const startOffset = Math.random() * 100; //Startposition längs skärmen
-    snowflake.style.left = `${startOffset}vw`;
+        const startOffset = Math.random() * 100; // Startposition längs skärmen
+        snowflake.style.left = `${startOffset}vw`;
 
-    const fallDuration = Math.random() * 5 + 5;//Fallhastighet för snöflingorna
-    snowflake.style.animation = `fall ${fallDuration}s linear infinite`;
+        const fallDuration = Math.random() * 5 + 5; // Fallhastighet för snöflingorna
+        snowflake.style.animation = `fall ${fallDuration}s linear infinite`;
 
-    cardBottom.appendChild(snowflake);
+        snow.appendChild(snowflake);
+      }
+    }
   }
-}
 
-createSnowflakes();
+  createSnowflakes();
+});
